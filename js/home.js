ba8db129f3dfb780f684ed531f4a88a21cde2169
Grete <greteveetla@gmail.com>
2022-04-16 11:50:08 +0300

    added some a href-links, css and js files

---
+++ js/home.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const url = 'http://localhost:3000'; // change url when uploading to server
+
+// select existing html elements
+const ul1 = document.getElementById('hrv-results');
+const ul2 = document.getElementById('stress survey results');
+
+// create kubios card
+
+
+// create area
+
+
+
+// AJAX call
+const getCat = async () => {
+    try {
+        const response = await fetch(url + '/user');
+        const user = await response.json();
+        createKubiosCard(user);
+    } catch (e) {
+        console.log(e.message);
+    }
+};
+getCat();
