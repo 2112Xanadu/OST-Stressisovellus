9d8d1d51155495fb1d502e889191542f9af34c87
Git-Joel <joeljj@metropolia.fi>
2022-05-03 13:31:57 +0300

    Add stressModel.js

---
+++ models/stressModel.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const pool = require("../database/db");
+const promisePool = pool.promise();
+
+const insertStressResult = async (stressResult) => {
+  try {
+    const [rows] = await promisePool.query(
+      "INSERT INTO stress (stressid, result, comment, userid) VALUES (?,?,?,?)",
+      [
+        stressResult.stressid,
+        stressResult.result,
+        stressResult.comment,
+        stressResult.userid,
+      ]
+    );
+    console.log("Stress result inserted!", rows);
+    return rows.insertId;
+  } catch (e) {
+    console.error("You have a problem: ", e.message);
+  }
+};
+// Get users latest stress result
+const getUserResult = async (id) => {
+  try {
+    const [rows] = await promisePool.query(
+      "SELECT result, comment, dateAndTime FROM stress WHERE userid = ?",
+      [id]
+    );
+    return rows;
+  } catch (e) {
+    console.error("stress model get result by id", e.message);
+  }
+};
+
+const getUserComment = async (id) => {
+  try {
+    const [rows] = await promisePool.query(
+      "SELECT comment FROM stress WHERE userid = ?",
+      [id]
+    );
+    return rows;
+  } catch (e) {
+    console.error("stress model get result by id", e.message);
+  }
+};
+
+module.exports = {
+  insertStressResult,
+  getUserResult,
+  getUserComment,
+};
